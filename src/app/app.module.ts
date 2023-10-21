9d3ea8d117068c89b2bb5a28c7e805ac93e8ebfe
Felipe Barroso Abdo <felipe.abdo@gmail.com>
2023-10-21 09:38:15 -0300

    configuração do tema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
