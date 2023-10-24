71f409098b1a9eb3a104015a4e8f2c3495823623
Gabriela Diniz <gadisa10@outlook.com>
2023-10-24 14:12:49 -0300

    style: ajuste identação

--- src/app/models/http/interface.ts
+++ src/app/models/http/interface.ts
@@ -2,20 +2,20 @@
  * Interfaces para a criação, leitura, atualização e exclusão de um objeto
  */
 export interface Create {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 export interface Read {
-    id: number;
-    email: string;
+  id: number;
+  email: string;
 }
 
 export interface Update {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 export interface Delete {
-    id: number;
+  id: number;
 }
