d53b1cb013402566a7faaaac4a2b9928584b4323
Gabriela Diniz <gadisa10@outlook.com>
2023-10-28 01:55:57 -0300

    chore: inclusão da rota cadastro sucesso/erro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RegisterSuccessComponent } from './components/register-success/register-success.component';
+import { RegisterFailedComponent } from './components/register-failed/register-failed.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomePageComponent
+  },
+  {
+    path: 'registrationSuccess',
+    component: RegisterSuccessComponent
+  },
+  {
+    path: 'registrationFailure',
+    component: RegisterFailedComponent
   },
   // Rota para capturar caminhos não correspondentes
   {
