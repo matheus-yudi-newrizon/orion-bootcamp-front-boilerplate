6ddd79cbe8c9f7ab9e7d800d89fd64b4463f05e3
Gabriela Diniz <gadisa10@outlook.com>
2023-10-28 02:05:57 -0300

    feat: início da integração de tela

--- src/app/components/form-register/form-register.component.ts
+++ src/app/components/form-register/form-register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-form-register',
@@ -10,7 +12,8 @@ export class FormRegisterComponent implements OnInit {
   emailPattern = /^[A-Za-z0-9.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
   registerForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
     /**
@@ -19,6 +22,7 @@ export class FormRegisterComponent implements OnInit {
     this.registerForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
       password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]]
     });
   }
 
@@ -44,8 +48,29 @@ export class FormRegisterComponent implements OnInit {
    * Obtém o valor da senha e atribui ao formulário
    * @param password
    */
-  public getPassword(password: string) {
-    console.log("Password value received:", this.registerForm);
+  public getPassword(password: string, confirmPassword: string) {
     this.registerForm.patchValue({ password: password });
+    this.registerForm.patchValue({ confirmPassword: confirmPassword });
+
+    console.log("aaa", this.registerForm.value)
+  }
+
+  /**
+   * Realiza o cadastro de um novo usuário e redireciona para as rotas de sucesso ou falha
+   * em caso de erro durante a criação da conta
+   * @throws {Error} Se houver algum problema durante o processo de criação da conta
+   */
+  public async createAccount(): Promise<void> {
+    try {
+      const value = await this.userService.signUp(this.registerForm.value);
+      const { success } = value;
+
+      const router = success ? '/registrationSuccess' : '/registrationFailure';
+      this.router.navigate([router]);
+    } catch (error) {
+      console.log("error: ", error);
+      this.router.navigate(['/registrationFailure']);
+      throw error;
+    }
   }
 }
