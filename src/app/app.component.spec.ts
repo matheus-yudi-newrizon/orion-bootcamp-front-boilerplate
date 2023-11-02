77127267bee0a735a15e770477cc9368c96b34c6
YudiMatheus <matheus.yudi@helpper.com.br>
2023-11-01 22:00:53 -0300

    chore: Changing boilerplate name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'review-reveal-frontend-orion-bootcamp'`, () => {
+  it(`should have as title 'review-reveal-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('review-reveal-frontend-orion-bootcamp');
+    expect(app.title).toEqual('review-reveal-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('review-reveal-frontend-orion-bootcamp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('review-reveal-web app is running!');
   });
 });
