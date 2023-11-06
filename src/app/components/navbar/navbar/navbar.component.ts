283fedc309f728ecb5329cfcaa58b0bfc7b78f69
Felipe Barroso Abdo <felipe.abdo@gmail.com>
2023-11-06 15:18:24 -0300

    página de login concluída para revisão

--- src/app/components/navbar/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
   currentRoute = '';
 
