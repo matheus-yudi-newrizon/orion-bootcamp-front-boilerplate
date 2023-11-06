507ad1f39afce7b7c87e7504f29b7f888e8ee629
Gabriela Diniz <gadisa10@outlook.com>
2023-11-06 16:05:59 -0300

    chore: validação do campo de e-mail

--- src/app/components/modal-forgot-password/modal-forgot-password.component.ts
+++ src/app/components/modal-forgot-password/modal-forgot-password.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { DialogData } from 'src/app/pages/register-page/register-page.component';
 
 @Component({
   selector: 'app-modal-forgot-password',
@@ -9,9 +9,13 @@ import { DialogData } from 'src/app/pages/register-page/register-page.component'
 })
 
 export class ModalForgotPasswordComponent {
+  emailPattern = /^[A-Za-z0-9.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+  forgotPasswordForm!: FormGroup;
 
-  constructor(public dialogRef: MatDialogRef<ModalForgotPasswordComponent>, @Inject(MAT_DIALOG_DATA) public data: DialogData) {
-
+  constructor(private formBuilder: FormBuilder, public dialogRef: MatDialogRef<ModalForgotPasswordComponent>, @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+    this.forgotPasswordForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern(this.emailPattern)]]
+    });
   }
 
   /**
@@ -19,5 +23,22 @@ export class ModalForgotPasswordComponent {
    */
   public closeModal(): void {
     this.dialogRef.close();
+  }
+
+  /**
+   * Função responsável por submeter o formulário e, em caso de ser inválido, não retornar nada
+   * @returns
+   */
+  public submit(): void {
+    if (!this.forgotPasswordForm.valid) {
+      return;
+    }
+  }
+
+  /**
+   * Função responsável por acessar o campo de e-mail do formulário
+  */
+  public get email(): FormControl {
+    return this.forgotPasswordForm.get('email') as FormControl;
   }
 }
