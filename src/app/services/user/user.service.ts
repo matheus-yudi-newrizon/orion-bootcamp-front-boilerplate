23f71abf7fb384b060e6a3cd7c38b343a4d98704
Felipe Barroso Abdo <felipe.abdo@gmail.com>
2023-11-10 19:16:41 -0300

    tela de login com correções feitas

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -10,10 +10,11 @@ interface SignUpRequest {
   confirmPassword: string;
 }
 
-interface SignUpResponse extends SuccessResponse<{
-  id: number;
-  email: string;
-}> { }
+interface SignUpResponse
+  extends SuccessResponse<{
+    id: number;
+    email: string;
+  }> {}
 
 interface PasswordResetRequest {
   email: string;
@@ -23,11 +24,23 @@ interface PasswordResetResponse extends SuccessResponse {
   message: string;
 }
 
+interface LoginRequest {
+  email: string;
+  password: string;
+  rememberMe: boolean;
+}
+
+interface LoginResponse
+  extends SuccessResponse<{
+    id: number;
+    email: string;
+    token: string;
+  }> {}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService extends RequestService {
-
   constructor(protected override httpClient: HttpClient) {
     super(httpClient);
   }
@@ -35,34 +48,67 @@ export class UserService extends RequestService {
   /**
    * Função responsável por realizar o cadastro de um novo usuário
    * @param data os dados do novo usuário a serem registrados
-   * @returns uma Promise contendo os dados do novo usuário ou um objeto ErrorResponse no caso de erro
+   * @returns uma Promise contendo os dados do novo usuário
    */
-  public async signUp(data: SignUpRequest): Promise<SignUpResponse | ErrorResponse> {
+  public async signUp(
+    data: SignUpRequest
+  ): Promise<SignUpResponse> {
     try {
-      return await lastValueFrom(this.httpClient.post<SignUpResponse>(this.BASE_URL + '/signup/', data))
+      return await lastValueFrom(
+        this.httpClient.post<SignUpResponse>(this.BASE_URL + '/signup/', data)
+      );
     } catch (error) {
       const errorResponse: ErrorResponse = {
         success: false,
-        message: "The user with email" + data.email + "already exists."
-      }
-      return errorResponse;
+        message: 'The user with email' + data.email + 'already exists.',
+      };
+      throw errorResponse;
     }
   }
 
   /**
    * Função responsável por enviar uma solicitação de redefinição de senha para o e-mail fornecido
    * @param email endereço de e-mail para o qual enviar a solicitação de redefinição de senha
-   * @returns Promise contendo os dados de retorno da solicitação de redefinição de senha ou um objeto ErrorResponse no caso de erro
+   * @returns Promise contendo os dados de retorno da solicitação de redefinição de senha
    */
-  public async forgotPassword(email: PasswordResetRequest): Promise<PasswordResetResponse | ErrorResponse> {
+  public async forgotPassword(
+    email: PasswordResetRequest
+  ): Promise<PasswordResetResponse> {
     try {
-      return await lastValueFrom(this.httpClient.post<PasswordResetResponse>(this.BASE_URL + '/forgot-password/', email))
+      return await lastValueFrom(
+        this.httpClient.post<PasswordResetResponse>(
+          this.BASE_URL + '/forgot-password/',
+          email
+        )
+      );
     } catch (error) {
       const errorResponse: ErrorResponse = {
         success: false,
-        message: "An error occurred while sending a password reset request. Please try again later."
-      }
-      return errorResponse;
+        message:
+          'An error occurred while sending a password reset request. Please try again later.',
+      };
+      throw errorResponse;
+    }
+  }
+
+  /**
+   * Função responsável por realizar o login de usuário
+   * @param data os dados do usuário qualquer
+   * @returns uma Promise contendo os dados do novo usuário
+   */
+  public async login (
+    data: LoginRequest
+  ): Promise<LoginResponse> {
+    try {
+      return await lastValueFrom(
+        this.httpClient.post<LoginResponse>(this.BASE_URL + '/login/', data)
+      );
+    } catch (error) {
+      const errorResponse: ErrorResponse = {
+        success: false,
+        message: 'Email or password is invalid.',
+      };
+      throw errorResponse;
     }
   }
 }
