6e0dccf6f42ea7531d771741ab3ca543b0b0598c
Felipe Barroso Abdo <felipe.abdo@gmail.com>
2023-11-11 00:05:37 -0300

    style: correções de estilo

--- src/app/components/navbar/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar/navbar.component.ts
@@ -5,31 +5,40 @@ import { PopUpHowToPlayService } from 'src/app/services/pop-up-how-to-play/pop-u
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
   private currentRoute = '';
 
-  constructor(private router: Router, public myService: PopUpHowToPlayService) {}
+  constructor(
+    private router: Router,
+    public myService: PopUpHowToPlayService
+  ) {}
 
   ngOnInit() {
     this.currentRoute = this.router.url;
   }
 
   /**
- * Verifica se a página atual é a página de login.
- * @returns True se a página atual é a página de login, caso contrário, False.
- */
+   * Verifica se a página atual é a página de login.
+   * @returns True se a página atual é a página de login, caso contrário, False.
+   */
   isLoginPage() {
     return this.currentRoute === '/login';
   }
   /**
-  * Verifica se a página atual é a página de cadastro.
-  * @returns True se a página atual é a página de cadastro, caso contrário, False.
-  */
+   * Verifica se a página atual é a página de cadastro.
+   * @returns True se a página atual é a página de cadastro, caso contrário, False.
+   */
   isSignUpPage() {
-    return this.currentRoute === '/sign-up' || '/reset-password';
+    return [
+      '/sign-up',
+      '/reset-password',
+      '/registration-success',
+      '/registration-failure',
+      '/reset-password-success',
+      '/reset-password-failure',
+    ].includes(this.currentRoute);
   }
 
   moverParaEsquerda(): void {
