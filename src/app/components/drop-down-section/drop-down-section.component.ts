972bed605919a2fa101fdbc33116d4ce7002d33c
Felipe Barroso Abdo <felipe.abdo@gmail.com>
2023-11-22 20:05:57 -0300

    versão 1.0

---
+++ src/app/components/drop-down-section/drop-down-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drop-down-section',
+  templateUrl: './drop-down-section.component.html',
+  styleUrls: ['./drop-down-section.component.scss']
+})
+export class DropDownSectionComponent {}
