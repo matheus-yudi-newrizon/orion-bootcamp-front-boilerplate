ad870759932cb398d2bfc28bf4d82273bf642801
Gabriela Diniz <gadisa10@outlook.com>
2023-11-28 21:51:05 -0300

    style: estilização do componente genérico do pop-up de acertos

--- src/app/components/pop-up-correct/pop-up-correct.component.ts
+++ src/app/components/pop-up-correct/pop-up-correct.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 export class PopUpCorrectComponent {
   public title = 'Congratulations!';
   public buttonLabel = 'NEXT MOVIE';
-  public movieName = '';
+  public movieName = 'Movie name';
 }
