7bff976f84bf6adad3b2456662b5e000aff1e64b
Gabriela Diniz <gadisa10@outlook.com>
2023-11-28 22:32:40 -0300

    chore: inclusão das funções genéricas do pop-up

--- src/app/components/pop-up-correct/pop-up-correct.component.ts
+++ src/app/components/pop-up-correct/pop-up-correct.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-pop-up-correct',
@@ -10,4 +11,15 @@ export class PopUpCorrectComponent {
   public buttonLabel = 'NEXT MOVIE';
   public imageUrl = '';
   public movieName = 'Movie name';
+
+  constructor(public dialogRef: MatDialogRef<void>) {}
+
+  /** Fecha o modal ao clicar no botão 'Close' */
+  public closeModal(): void {}
+
+  /* Redireciona o usuário para o site do TMDB */
+  public seeMoreMovie(): void {}
+
+  /** Leva o usuário a uma nova tentativa no jogo */
+  public nextMovie(): void {}
 }
--- src/app/components/pop-up-success-confirmation/pop-up-success-confirmation.component.ts
+++ src/app/components/pop-up-success-confirmation/pop-up-success-confirmation.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { PopUpCorrectComponent } from '../pop-up-correct/pop-up-correct.component';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pop-up-success-confirmation',
@@ -11,4 +13,24 @@ export class PopUpSuccessConfirmationComponent extends PopUpCorrectComponent {
   public override buttonLabel = 'Next movie';
   public override imageUrl!: string;
   public override movieName!: string;
+
+  constructor(
+    public override dialogRef: MatDialogRef<void>,
+    public router: Router
+  ) {
+    super(dialogRef);
+  }
+
+  /** Função para fechar o modal ao clique */
+  public override closeModal(): void {
+    this.dialogRef.close();
+  }
+
+  /** Função para ver mais informações sobre um filme */
+  public override seeMoreMovie(): void {}
+
+  /** Função para navegar para a próxima tela de jogo */
+  public override nextMovie(): void {
+    this.router.navigate(['/game']);
+  }
 }
