9fdd24a90248a1ff9502958253448d554435f9a0
Gabriela Diniz <gadisa10@outlook.com>
2023-12-04 20:27:56 -0300

    style: estilização pop-up game over

--- src/app/components/pop-up-game-over/pop-up-game-over.component.ts
+++ src/app/components/pop-up-game-over/pop-up-game-over.component.ts
@@ -6,4 +6,7 @@ import { PopUpGameComponent } from '../pop-up-game/pop-up-game.component';
   templateUrl: '../pop-up-game/pop-up-game.component.html',
   styleUrls: ['../pop-up-game/pop-up-game.component.scss', './pop-up-game-over.component.scss']
 })
-export class PopUpGameOverComponent extends PopUpGameComponent {}
+export class PopUpGameOverComponent extends PopUpGameComponent {
+  public override title = 'Game Over!!';
+  public override buttonLabel = 'RESTART';
+}
