c9c70f2de1a19f9360018ea6484dd223b72b50a0
Gabriela Diniz <gadisa10@outlook.com>
2023-12-04 23:39:14 -0300

    chore: refatorando componente register-failed pra componentização

--- src/app/components/register-failed/register-failed.component.ts
+++ src/app/components/register-failed/register-failed.component.ts
@@ -1,44 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { ReturnConfirmationComponent } from '../return-confirmation/return-confirmation.component';
 
 @Component({
   selector: 'app-register-failed',
-  templateUrl: './register-failed.component.html',
-  styleUrls: ['./register-failed.component.scss']
+  templateUrl: '../return-confirmation/return-confirmation.component.html',
+  styleUrls: ['../return-confirmation/return-confirmation.component.scss']
 })
-export class RegisterFailedComponent implements OnInit {
-  public titlePageFailed = '';
-  public textPageFailed = '';
-  public currentUrl = '';
+export class RegisterFailedComponent extends ReturnConfirmationComponent {
+  public override titlePageSuccess = 'It was not possible to create your account';
+  public override belowTheTitle = 'Please return to the home page and try again';
+  public override labelButton = 'RETURN TO HOME';
+  public override imgURL = 'icon-error.svg';
 
-  constructor(private router: Router) {}
-
-  public ngOnInit(): void {
-    this.currentUrl = this.router.url;
-    this.changeWordsComponent();
-  }
-
-  /**
-   * Retorna para a tela de cadastro em caso de erro.
-   */
-  public returnToHome(): void {
-    if (this.currentUrl === '/registration-failure') {
-      this.router.navigate(['/sign-up']);
-    } else if (this.currentUrl === '/reset-password-failure') {
-      this.router.navigate(['/login']);
-    }
-  }
-
-  /**
-   * Função responsável por mudar o título e textos da página baseado na URL.
-   */
-  public changeWordsComponent(): void {
-    if (this.currentUrl === '/registration-failure') {
-      this.titlePageFailed = 'It was not possible to create your account';
-      this.textPageFailed = 'Please return to the home page and try again.';
-    } else if (this.currentUrl === '/reset-password-failure') {
-      this.titlePageFailed = 'It was not possible to recover the password';
-      this.textPageFailed = 'Please return to the home page and request again.';
-    }
+  public override return(): void {
+    this.router.navigate(['/sign-up']);
   }
 }
