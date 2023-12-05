de6c9dd6df0940118a31d9b2b35453ee84fc1102
Gabriela Diniz <gadisa10@outlook.com>
2023-12-04 23:39:39 -0300

    chore: refatorando componente register-success pra componentização

--- src/app/components/register-success/register-success.component.ts
+++ src/app/components/register-success/register-success.component.ts
@@ -1,40 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { ReturnConfirmationComponent } from '../return-confirmation/return-confirmation.component';
 
 @Component({
   selector: 'app-register-success',
-  templateUrl: './register-success.component.html',
-  styleUrls: ['./register-success.component.scss']
+  templateUrl: '../return-confirmation/return-confirmation.component.html',
+  styleUrls: ['../return-confirmation/return-confirmation.component.scss']
 })
-export class RegisterSuccessComponent implements OnInit {
-  public titlePageSuccess = '';
-  public currentUrl = '';
-
-  constructor(private router: Router) {}
-
-  /* Capturando a URL na inicialização do componente e chamando a função para alterar o título */
-  public ngOnInit(): void {
-    this.currentUrl = this.router.url;
-    this.changeTitleComponent();
-  }
-
-  /**
-   * Função responsável por mudar o título da página baseado na URL
-   */
-  public changeTitleComponent(): void {
-    if (this.currentUrl === '/registration-success') {
-      this.titlePageSuccess = 'Account created successfully';
-    } else if (this.currentUrl === '/reset-password-success') {
-      this.titlePageSuccess = 'Password reset successfully';
-    }
-  }
-
-  /**
-   * Retorna para a tela de cadastro em caso de sucesso.
-   */
-  public returnToHome(): void {
-    if (this.currentUrl === '/registration-success' || this.currentUrl === '/reset-password-success') {
-      this.router.navigate(['/login']);
-    }
-  }
+export class RegisterSuccessComponent extends ReturnConfirmationComponent {
+  public override titlePageSuccess = 'Account created successfully';
+  public override belowTheTitle = 'You can now play the game.';
+  public override labelButton = 'PLAY NOW';
 }
