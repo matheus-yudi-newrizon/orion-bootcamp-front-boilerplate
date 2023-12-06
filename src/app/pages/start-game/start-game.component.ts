cb20b32ea0333b5343f6a3eeae762c88a94b255d
Gabriela <gadisa10@outlook.com>
2023-12-06 13:54:27 -0300

    feat: teste alterar label do botão start-game

--- src/app/pages/start-game/start-game.component.ts
+++ src/app/pages/start-game/start-game.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { TokenService } from 'src/app/services/token/token.service';
 import { UserService } from 'src/app/services/user/user.service';
-
 @Component({
   selector: 'app-start-game',
   templateUrl: './start-game.component.html',
@@ -10,12 +9,18 @@ import { UserService } from 'src/app/services/user/user.service';
 })
 export class StartGameComponent {
   public isLoading = false;
+  public labelButton = 'START GAME';
+  public resultStart: any;
 
   constructor(
     public router: Router,
     private userService: UserService,
     private tokenService: TokenService
   ) {}
+
+  public ngOnInit(): void {
+    this.activeGame();
+  }
 
   /**
    * Função responsável por dar play no jogo, redirecionando para a página do game.
@@ -31,12 +36,24 @@ export class StartGameComponent {
         const data = { token: token };
         const response = await this.userService.startGame(data);
         this.tokenService.saveGameData(response.data!);
+        this.resultStart = response.data;
       }
 
       this.router.navigate(['/game']);
     } catch (error) {
       this.isLoading = false;
       console.error('error', error);
+    }
+  }
+
+  /**
+   * Verifica se o usuário tem algum jogo ativo e altera a label do botão.
+   */
+  public activeGame(): void {
+    if (this.resultStart.isActive) {
+      this.labelButton = 'CONTINUE';
+    } else {
+      this.labelButton = 'START GAME';
     }
   }
 }
